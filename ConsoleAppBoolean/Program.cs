64a1ac88c5304293444e7f3c3488a7e56379f023
Rushikesh Bhattad <rushikeshbhattad8@gmail.com>
2021-08-30 14:36:31 +0530

    Add project files.

---
+++ ConsoleAppBoolean/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace BooleanDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool result;
+            int x = 10;
+            result = (!(x == 10));
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
